c169b88cb0997a35faf592c70edbe7d2ddd33a24
mostafijur <mrmoon025@gmail.com>
2025-04-13 09:12:52 +0600

    auth_api

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Auth API Documentation",
+ *      description="Auth API description",
+ *      @OA\Contact(
+ *          email="mostafijur.til@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ */
+abstract class Controller
+{
+    //
+}
---
+++ app/Http/Middleware/CorsMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CorsMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // $allowedOrigin = 'http://localhost:5173';
+
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With, Authorization',
+            'Access-Control-Allow-Credentials' => 'true'
+        ];
+
+        if ($request->isMethod('OPTIONS')) {
+            return response()->json('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->headers->set($key, $value);
+        }
+
+        return $response;
+    }
+}
