c17eb0f5c05feb7881cc727a6537866994c661c5
mostafijur <mrmoon025@gmail.com>
2025-04-20 10:49:55 +0600

    [feat] Added pusher.

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,13 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Broadcast;
 
-Artisan::command('inspire', function () {
-    /** @var ClosureCommand $this */
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     /** @var ClosureCommand $this */
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
+
+Broadcast::channel('chat.{receiverId}', function ($user, $receiverId) {
+    return (int) $user->id === (int) $receiverId;
+});
