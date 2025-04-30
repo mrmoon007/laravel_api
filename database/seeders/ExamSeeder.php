6f647ec074ced4241385c4496687130b46fbecb1
mostafijur <mrmoon025@gmail.com>
2025-04-30 09:14:40 +0600

    [feat] Added exam model, migration & seeder.

---
+++ database/seeders/ExamSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Exam;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ExamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Exam::create([
+            'title' => 'EXAM-1',
+            'duration_minutes' => 10
+        ]);
+    }
+}
